1b33e21690750f06e9cbcbb9d96bbfe37c7ceb9a
julia cwynar <jcwynar@student.agh.edu.pl>
2023-11-20 22:28:04 +0100

    functional prototype update date edit

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Post name="Stage 1: Project Proposal" date="03/10/23" document="https://drive.google.com/file/d/1cDVKD8tqvwPl48TttJS6wqBSLU0QHbv9/view?usp=sharing" />
       <Post name="Stage 2: User and Task Analysis" date="07/10/23" document="https://drive.google.com/file/d/1e8SE0QEwX533qaDziHBTLmdxyLQHaFrV/view?usp=sharing" />
       <Post name="Stage 3: First Prototype" date="21/10/23" document="https://drive.google.com/file/d/1_b4rQD2kqOt9KcaMJSzlnNjG8pFSHpzM/view" />
-      <Post name="Stage 4: Functional Prototype" date="21/10/23" document="https://drive.google.com/file/d/1ZCJk37RWpI0nQiO8tGDZHVjcQNbE0vLJ/view" />
+      <Post name="Stage 4: Functional Prototype" date="20/11/23" document="https://drive.google.com/file/d/1ZCJk37RWpI0nQiO8tGDZHVjcQNbE0vLJ/view" />
     </div>
     </div>
   </React.StrictMode>,
